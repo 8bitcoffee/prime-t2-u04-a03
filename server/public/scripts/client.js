2ba7561b167fdd60806bf4c76d1386115073211e
J Read <j@8bit.coffee>
2023-10-05 12:14:15 -0500

    part one base complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,123 @@
 console.log( 'js' );
 
+let id = 1999;
+const nameIn = document.querySelector('#nameIn');
+const ageIn = document.querySelector('#ageIn');
+const genderIn = document.querySelector('#genderIn');
+const readyForTransferIn = document.querySelector('#readyForTransferIn');
+const notesIn = document.querySelector('#notesIn');
+const viewKoalas = document.querySelector('#viewKoalas');
+
+// GET REQUEST
 function getKoalas(){
   console.log( 'in getKoalas' );
-  // axios call to server to get koalas
-  
+  // axios GET call to server to get koalas
+  axios.get('/koalas').then((response) => {
+    console.log(response.data);
+    // koalas array assigned to variable
+    let koalas = response.data;
+    // koala string to append to DOM has been cleared
+    let viewKoalasStr = "";
+    // clear your innerHTML table
+    viewKoalas.innerHTML = viewKoalasStr;
+    
+    for (let koala of koalas) {
+
+      if(koala.readyToTransfer == 'N'){
+        viewKoalasStr += `
+          <tr>
+            <td>${koala.name}</td>
+            <td>${koala.gender}</td>
+            <td>${koala.age}</td>
+            <td><button onclick='readyKoala(${koala.id})'>Ready for Transfer</button></td>
+            <td>${koala.notes}</td>
+            <td><button onClick='removeKoala(${koala.id})'>Delete</button>
+          </tr>
+        `;
+      }
+      else{
+        // loop through your koala table data and add to viewKoalaStr
+        viewKoalasStr += `
+          <tr>
+            <td>${koala.name}</td>
+            <td>${koala.gender}</td>
+            <td>${koala.age}</td>
+            <td></td>
+            <td>${koala.notes}</td>
+            <td><button onClick='removeKoala(${koala.id})'>Delete</button>
+          </tr>
+        `;
+      }
+      
+    }
+    // append your viewKoalasStr to the DOM
+    viewKoalas.innerHTML = viewKoalasStr;
+    
+  }).catch((error) => {
+    // include your catch
+    console.error(error);
+    alert('Koala GET failed');
+  });
 } // end getKoalas
 
-function saveKoala(){
+// POST REQUEST
+function saveKoala(event){
+  event.preventDefault();
   console.log( 'in saveKoala' );
-  // axios call to server to get koalas
- 
+  // axios POST call to server to get koalas
+  // obtain values from input fields
+  let name = nameIn.value;
+  let age = ageIn.value;
+  let gender = genderIn.value;
+  let transfer = readyForTransferIn.value;
+  let notes = notesIn.value;
+  
+  console.log(`Name: ${name}, Age: ${age}, Gender: ${gender}, Ready for Transfer: ${transfer}, Notes: ${notes}`)
+  // begin POST match '/savekoalas'
+  axios.post('/koalas', {
+    // object to be passed to array
+    id : id,
+    name: name,
+    age: age,
+    gender: gender,  
+    readyToTransfer: transfer,
+    notes: notes
+  }).then((response) => {
+    console.log('Koala transfer successful');
+    getKoalas();
+  }).catch((error) => {
+    // dont forget the .catch
+    console.error(error);
+    alert(`Koalas not added, something went wrong!`)
+  });
+// increment your id to label your koalas
+  id += 1
 }
 
 getKoalas();
+
+// UPDATE REQUEST
+function readyKoala(id) {
+  console.log('Koala to make ready', id);
+  axios.put(`/koalas/${id}`, {
+    readyToTransfer: 'Y'
+  }).then((response) =>{
+    console.log(`Koala transfer updated`);
+    getKoalas();
+  }).catch((error) => {
+    console.log(error);
+    alert(`Could not update Ready to Transfer ${id}`)
+  })
+}
+
+// DELETE REQUEST
+function removeKoala(id){
+  console.log('Koala to remove: ', id);
+  axios.delete(`/koalas/${id}`).then(() => {
+    console.log('You successfully terminated that koala');
+    getKoalas();
+  }).catch((error) => {
+    console.log(error);
+    alert(`your koala was not deleted`);
+  })
+}
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,17 +2,77 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+let koalas = [{
+    id: 1,
+    name: 'Scotty',
+    gender: 'M',
+    age: 4,
+    readyToTransfer: 'Y',
+    notes: 'Born in Guatemala'
+},
+{
+    id: 2,
+    name: 'BoBody',
+    gender: 'F',
+    age: 19,
+    readyToTransfer: 'N',
+    notes: 'Loves tacos'
+},{
+    id: 3,
+    name: 'Ororo',
+    gender: 'F',
+    age: 7,
+    readyToTransfer: 'N',
+    notes: 'Loves listening to Pantera'
+},
+{   id: 4,
+    name: 'Mr. Bubbles',
+    gender: 'M',
+    age: 68,
+    readyToTransfer: 'Y',
+    notes: 'Once met President Clinton, never shuts up about it'
+},
+{
+    id: 5,
+    name: 'Charlie',
+    gender: 'M',
+    age: 9,
+    readyToTransfer: 'Y',
+    notes: 'Bit my finger'
+}
+];
 
 // GET
-
+koalaRouter.get('/', (req,res) =>{
+    console.log('GET request made for koalas');
+    // response of [koalas]
+    res.send(koalas);
+})
 
 // POST
-
+koalaRouter.post(('/'), (req,res) =>{
+    console.log(req.body);
+    // send new object to [koalas]
+    koalas.push(req.body);
+    // send your status back to client
+    res.sendStatus(201);
+    console.log(koalas);
+})
 
 // PUT
-
+koalaRouter.put('/:id', (req, res) => {
+    let idx = koalas.findIndex(koala => koala.id == req.params.id);
+    koalas[idx].readyToTransfer = 'Y';
+    console.log(koalas);
+    
+    res.sendStatus(200);
+});
 
 // DELETE
+koalaRouter.delete('/:id', (req,res) =>{
+    koalas = koalas.filter(koala => koala.id != req.params.id);
+    console.log(koalas);
+    res.sendStatus(200);
+});
 
 module.exports = koalaRouter;
